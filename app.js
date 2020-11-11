b20c0a016c2859ca5ea47100970ae409f3ad8407
Wilson Ramos <hupcontato@gmail.com>
2020-11-11 17:20:44 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const { static } = require('express')
+const express = require('express')
+const app = express()
+
+app.use((req, res, next) => { 
+    if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) 
+        res.redirect(`https://${req.headers.host}${req.url}`);
+    else  
+        next(); 
+});
+app.use(express.static('public'))
+
+app.get('/', (req, res)=> {
+    res.sendFile(__dirname + '/html/index.html')
+})
+
+app.listen(3000, ()=> {
+    console.log('Rodando 3000')
+})
