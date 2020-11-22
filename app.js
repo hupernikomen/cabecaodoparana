a3d3f1f5f8cc1b8bd1fc4a3036a158f41c85938a
Wilson Ramos <hupcontato@gmail.com>
2020-11-22 10:08:08 -0300

    primeiro commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const { static } = require('express')
 const express = require('express')
+
 const app = express()
 
 app.use((req, res, next) => { 
@@ -11,9 +11,12 @@ app.use((req, res, next) => {
 app.use(express.static('public'))
 
 app.get('/', (req, res)=> {
+    res.sendFile(__dirname + '/html/manutencao.html')
+})
+app.get('/home', (req, res)=> {
     res.sendFile(__dirname + '/html/index.html')
 })
 
-app.listen(3000, ()=> {
+app.listen(3000, ()=>{
     console.log('Rodando 3000')
 })
